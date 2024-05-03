7e96ede3e1f5ab359ab386cf416cf884f46fd84c
Vainzald <aalejandro200327@gmail.com>
2024-05-03 16:12:29 -0400

    Cambios

--- src/components/contacto/contacto.jsx
+++ src/components/contacto/contacto.jsx
@@ -1,4 +1,4 @@
-import { BsInstagram } from "react-icons/bs";
+import { BsInstagram, BsWhatsapp } from "react-icons/bs";
 import { CiMail } from "react-icons/ci";
 import { FaFacebookF } from "react-icons/fa6";
 import { GrFacebook } from "react-icons/gr";
@@ -17,15 +17,16 @@ export const Contactos = () => {
                 </div>
                 <div className="col-md-8 align-content-center">
                     <div className="card-body">
-                        <span className="card-text">
-                            <a className="text-decoration-none card-link card-text text-light" href="https://www.facebook.com/profile.php?id=61558231736069&mibextid=ZbWKwL" target="_blank" style={{color: "#c0c0c0;"}}><SiFacebook size={'25px'} /> Shalom soldadura . artesania rústico moderno . video vigilancia </a>
-                        </span>
                         <p className="card-text">
-                            {/* <a className="text-decoration-none card-link card-text" href=""><BsInstagram size={'25px'}/>  </a>  */}
+                            <a className="text-decoration-none card-link card-text text-light" href="https://www.facebook.com/profile.php?id=61558231736069&mibextid=ZbWKwL" target="_blank" style={{color: "#c0c0c0;"}}><SiFacebook size={'25px'} /> Shalom soldadura . artesania rústico moderno . video vigilancia </a>
                         </p>
-                        <span className="card-text ">
-                            <a className="text-decoration-none card-link card-text text-light" href="mailto:shalommavem@gmail.com"><IoIosMail size={'25px'} target="_blank"/> shalommavem@gmail.com</a>
-                        </span>
+                        <p className="card-text">
+                            <a className="text-decoration-none card-link card-text text-light" href="https://api.whatsapp.com/send?phone=56957598100" target="_blank"><BsWhatsapp size={'25px'} /> +56 9 5759 8100</a>
+                        </p>
+                        <p className="card-text">
+                            <a className="text-decoration-none card-link card-text text-light" href="mailto:shalommavem@gmail.com" target="_blank"><IoIosMail size={'25px'} /> shalommavem@gmail.com</a>
+                        </p>
+                        
                         
                         
                     </div>
